09780e8bdc7b7c26fe5cf86eca998bc58669e0c7
Andres Rovira <wrovira@paypal.com>
2023-06-14 09:44:34 -0600

    [initial-import] Initial import

---
+++ src/test/java/com/github/acwilan/weather/forecasting/WeatherForecastingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.acwilan.weather.forecasting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherForecastingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
