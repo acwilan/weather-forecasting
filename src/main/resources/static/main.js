9a89984fb67fada75efbd36ebc7434759bf15a6e
Andres Rovira <wrovira@paypal.com>
2023-06-14 18:10:54 -0600

    [3-weather-forecasting-application] clean up resources and fix frontend display

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -2,9 +2,8 @@ $(document).ready(function(){
     $('#searchForm').submit(function(event){
         event.preventDefault();
         var searchTerm = $('#location').val();
-        var url = "https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyCOVakodpYAD9sQ71nldMXJq5HfFHCE6FU&address=" + searchTerm;
+        var url = "https://maps.googleapis.com/maps/api/geocode/json?key=" + gconfig.key + "&address=" + searchTerm;
         $.getJSON(url, function(data){
-            console.log(data);
             var lat = data.results[0].geometry.location.lat;
             var lng = data.results[0].geometry.location.lng;
             initMap(data.results[0].formatted_address, lat, lng)
@@ -15,11 +14,22 @@ $(document).ready(function(){
 async function initMap(location, lat, lng) {
   const { Map } = await google.maps.importLibrary("maps");
   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  const position = { lat: lat, lng: lng };
+
   const map = new Map(document.getElementById("map"), {
-    center: { lat: lat, lng: lng },
+    center: position,
     zoom: 8,
     mapId: 'ab321b800b5e8a1'
   });
+
+  // The marker, positioned at location
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: location,
+  });
+
   $.ajax({
     url: '/api.weather.local',
     type: 'POST', 
@@ -27,32 +37,25 @@ async function initMap(location, lat, lng) {
     contentType: 'application/json; charset=utf-8',
     dataType: 'json', 
     success: function(data){
-        const form = document.createElement('div');
-        form.className = 'form-horizontal';
-        $(form).append(
+        console.log(data);
+        $('#weather-information').empty().append(
             $('<div class="form-group"/>').append(
                 $('<label class="col-sm-2 control-label">Temperature</label>'),
-                $('<div class="col-sm-10"><p class="form-control-static">' + data.temperature + '°C</p></div>')
+                $('<div class="col-sm-10"><p class="form-control-static">' + data.temperature + 'F</p></div>')
             ),
             $('<div class="form-group"/>').append(
                 $('<label class="col-sm-2 control-label">Humidity</label>'),
-                $('<div class="col-sm-10"><p class="form-control-static">' + data.humidity + '°C</p></div>')
+                $('<div class="col-sm-10"><p class="form-control-static">' + data.humidity + '%</p></div>')
             ),
             $('<div class="form-group"/>').append(
                 $('<label class="col-sm-2 control-label">Wind Speed</label>'),
-                $('<div class="col-sm-10"><p class="form-control-static">' + data.windSpeed + '°C</p></div>')
+                $('<div class="col-sm-10"><p class="form-control-static">' + data.windSpeed + '</p></div>')
             ),
             $('<div class="form-group"/>').append(
                 $('<label class="col-sm-2 control-label">Condition</label>'),
-                $('<div class="col-sm-10"><p class="form-control-static">' + data.condition + '°C</p></div>')
+                $('<div class="col-sm-10"><p class="form-control-static">' + data.condition + '</p></div>')
             ),
         );
-        console.log(form.outerHTML);
-        const marker = new AdvancedMarkerElement({
-            map,
-            position: {lat: lat, lng: lng},
-            content: form
-        });
     }
   });
 }
